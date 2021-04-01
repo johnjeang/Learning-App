0340f5109929c0341b6db69baff044a0d120bb1c
johnjeang <johnjeang@gmail.com>
2021-04-01 12:31:51 -0500

    Parse json and html in Content model

--- Learning-App/ViewModels/ContentModel.swift
+++ Learning-App/ViewModels/ContentModel.swift
@@ -9,4 +9,52 @@ import Foundation
 
 class ContentModel: ObservableObject{
     
+    @Published var modules = [Module]()
+    var styleData: Data?
+    
+    init(){
+        getLocalData()
+    }
+    
+    func getLocalData(){
+        // get path
+        
+        // get url
+        let jsonURL = Bundle.main.url(forResource: "data", withExtension: "json")
+    
+        
+        // get data object
+        do{
+            let jsonData = try Data(contentsOf: jsonURL!)
+            
+            let jsonDecoder = JSONDecoder()
+            
+            // decode data object
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            self.modules = modules
+        }
+        catch{
+            print("Oops, there was an error trying to parse the local data")
+        }
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+        }
+        catch {
+            print("Oops there was an error why trying to parse the style information")
+        }
+        
+        
+
+        
+
+        
+        
+    }
+    
 }
